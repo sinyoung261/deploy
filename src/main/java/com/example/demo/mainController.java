ffab6df617fbe9efcdfaee12972233a391d370bd
sinyoung261 <waterfired@naver.com>
2025-03-28 11:24:52 +0900

    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

---
+++ src/main/java/com/example/demo/mainController.java
@@ -0,0 +1,19 @@
+package com.example.demo;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class mainController {
+	
+	@Value("{file.upload}")
+	String upload;
+	
+	@GetMapping("/")
+	@ResponseBody
+	public String main() {
+		return "hello :" + upload;
+	}
+}
